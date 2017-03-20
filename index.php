1da7881d3b98f07b61ca53b7ae60b2d893fc174d
Stephen Reynolds <sgrjr@deliverance.me>
2017-03-19 20:42:05 -0400

    minor changes

--- index.php
+++ index.php
@@ -1,9 +1,8 @@
 <?php
-header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
 
-require __DIR__.'/../bibleexchange/bootstrap/autoload.php';
+require __DIR__.'/../bootstrap/autoload.php';
 
-$app = require_once __DIR__.'/../bibleexchange/bootstrap/app.php';
+$app = require_once __DIR__.'/../bootstrap/app.php';
 
 $kernel = $app->make('Illuminate\Contracts\Http\Kernel');
 $response = $kernel->handle(
