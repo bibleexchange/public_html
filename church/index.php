b3cf34e3d3f376f660c5834fd2c2d7fcb11c5c13
Stephen Reynolds <sgrjr@deliverance.me>
2019-06-08 21:06:56 -0400

    fixed church

---
+++ church/index.php
@@ -0,0 +1,3 @@
+<?php
+include('app/App.php');
+?>
--- index.php
+++ index.php
@@ -1,3 +1,9 @@
 <?php
-include('app/App.php');
-?>
+require __DIR__.'/../bibleexchange/bootstrap/autoload.php';
+$app = require_once __DIR__.'/../bibleexchange/bootstrap/app.php';
+$kernel = $app->make('Illuminate\Contracts\Http\Kernel');
+$response = $kernel->handle(
+	$request = Illuminate\Http\Request::capture()
+);
+$response->send();
+$kernel->terminate($request, $response);
