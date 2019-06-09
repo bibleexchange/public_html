d0b23f082813eab2acf2f20abeff3a1803e0554b
sgrjr <sgrjr@deliverance.me>
2019-06-09 04:31:26 -0700

    side

---
+++ church/posts/main-sidebar.php
@@ -0,0 +1,18 @@
+<a href="/"><img id="logo" src="/img/logo.jpg" /></a>
+<h1>$business_name </h1>
+
+<div class="mixlr"></div>
+
+<div id="message-board" style="display:none">
+	<iframe name="mixlr-player" src="http://mixlr.com/v2/embed/deliverance-center?width=640" scrolling="no" frameborder="no" height="150" width="100%"></iframe>
+</div>
+<p>930 Old Post Rd Arundel, Maine 04046</p>
+<p>Call or Text: (207) 774-8192</p>
+
+<a>Links</h2>
+<ul>
+	<a href="/">Home</a>
+	<li><a href="/blog">Blog</a></li>
+	<li><a href="http://mixlr.com/deliverance-center/show-reel">Archive Service Recordings on Mixlr</a></li>
+	<li><a href="https://www.facebook.com/pg/deliverancecenter">Facebook</a></li>
+</ul>
